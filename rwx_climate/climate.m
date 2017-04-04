2c73ea113116f5914e7a227b1584e95254957ee1
Joshua S <j.soderholm@uq.edu.au>
2017-04-04 16:56:21 +1000

    implemented track swaths, but slow

--- rwx_climate/climate.m
+++ rwx_climate/climate.m
@@ -35,6 +35,9 @@ load([local_tmp_path,global_config_fn,'.mat'])
 
 % Load site info
 read_site_info(site_info_fn); load([local_tmp_path,site_info_fn,'.mat']);
+site_ind = find(siteinfo_id_list==radar_id);
+site_lat = siteinfo_lat_list(site_ind);
+site_lon = siteinfo_lon_list(site_ind);
 
 % build transforms
 transform_path    = [local_tmp_path,'transforms/'];
@@ -68,7 +71,7 @@ if date_list_flag == 1
 end
 %create date mask
 date_mask        = ismember(floor(storm_date_list),date_list);
-%% mask cells by var, time and date
+%% mask cells by var, time and distance
 %extract mask var
 switch data_type
     case 'mesh'
@@ -91,19 +94,23 @@ switch data_type
 		stormh5_varname = 'sts_h_grid';
 end
 %create var mask
-if strcmp(data_min,'nan')
+if strcmp(stat_min,'nan')
     min_mask = true(length(stat_var),1);
 else
-    min_mask = stat_var >= data_min;
+    min_mask = stat_var >= stat_min;
 end
-if strcmp(data_max,'nan')
+if strcmp(stat_max,'nan')
     max_mask = true(length(stat_var),1);
 else
-    max_mask = stat_var >= data_max;
+    max_mask = stat_var >= stat_max;
 end
 %create time mask
 time_list      = rem(storm_date_list,1);
 time_mask      = time_list>=rem(datenum(time_min,'HH:MM'),1) & time_list<=rem(datenum(time_max,'HH:MM'),1);
+%create distance mask
+[arclen,~] = distance(site_lat,site_lon,storm_latloncent_list(:,1),storm_latloncent_list(:,2));
+dist_list  = deg2km(arclen);
+dist_mask  = dist_list <= data_range;
 
 %% ci/ce mask
 if ci_flag == 1 || ce_flag == 1
@@ -158,30 +165,27 @@ else
     ci_ce_mask      = true(length(storm_date_list),1);
 end
 
-%% combine masks
-data_mask      = min_mask & max_mask & time_mask & date_mask & ci_ce_mask;
+% combine masks
+data_mask      = min_mask & max_mask & time_mask & date_mask & ci_ce_mask & dist_mask;
 
-%% create plots
+%% Plotting
 
 %load transform
 transform_fn = [transform_path,'regrid_transform_',num2str(radar_id,'%02.0f'),'.mat'];
 load(transform_fn,'geo_coords','grid_size','img_latlonbox')
-
-%create blank grids
-cent_lat_vec = img_latlonbox(1):-centroid_grid:img_latlonbox(2);
-cent_lon_vec = img_latlonbox(4):centroid_grid:img_latlonbox(3);
-cent_grid    = zeros(length(cent_lat_vec),length(cent_lon_vec));
-max_grid     = zeros(grid_size(1),grid_size(2));
-mean_grid    = zeros(grid_size(1),grid_size(2));
-density_grid = zeros(grid_size(1),grid_size(2));
-dir_u_grid   = zeros(grid_size(1),grid_size(2));
-dir_v_grid   = zeros(grid_size(1),grid_size(2));
-
 %apply masks
 storm_date_list       = storm_date_list(data_mask);
 storm_trck_list       = storm_trck_list(data_mask);
-storm_latloncent_list = storm_latloncent_list(find(data_mask),:);
+storm_latloncent_list = storm_latloncent_list(data_mask,:);
+storm_subset_list     = storm_subset_list(data_mask);
+storm_ijbox_list      = storm_ijbox_list(data_mask,:);
+% centroid plot
 
+%allocate
+cent_lat_vec = img_latlonbox(1):-centroid_grid:img_latlonbox(2);
+cent_lon_vec = img_latlonbox(4):centroid_grid:img_latlonbox(3);
+cent_grid    = zeros(length(cent_lat_vec),length(cent_lon_vec));
+%bin 
 for i=1:length(storm_date_list)
     temp_lat = storm_latloncent_list(i,1);
     temp_lon = storm_latloncent_list(i,2);
@@ -189,13 +193,124 @@ for i=1:length(storm_date_list)
     [~,lon_ind] = min(abs(cent_lon_vec - temp_lon));
     cent_grid(lat_ind,lon_ind) = cent_grid(lat_ind,lon_ind)+1;
 end    
-
-cent_grid(cent_grid>12)=12;
-
 %plot centroid grid
 R = makerefmat('RasterSize',[length(cent_lat_vec),length(cent_lon_vec)],'LatitudeLimits',[min(cent_lat_vec) max(cent_lat_vec)],'LongitudeLimits',[min(cent_lon_vec) max(cent_lon_vec)]);
 generate_map(cent_grid,R,map_config_fn)
 
+% Density/Direction plots
+
+%allocate
+blank_grid  = zeros(grid_size(1),grid_size(2));
+track_grids = struct('density_grid',blank_grid,'u_grid',blank_grid,'v_grid',blank_grid);
+
+flr_storm_date_list  = floor(storm_date_list);
+uniq_storm_date_list = unique(flr_storm_date_list);
+for i=1:length(uniq_storm_date_list) %loop through each date
+    %extract current day
+    target_date = uniq_storm_date_list(i);
+    disp(datestr(target_date));
+    date_ind    = find(flr_storm_date_list==target_date);
+    track_list  = storm_trck_list(date_ind);
+    %remove no tracks
+    remove_ind  = track_list==0;
+    date_ind(remove_ind)   = [];
+    track_list(remove_ind) = [];
+    %skip if no tracks left
+    if isempty(track_list)
+        continue
+    end
+    %loop through each track id
+    uniq_track_list = unique(track_list);
+    for j=1:length(uniq_track_list)
+        %extract current track
+        target_track = uniq_track_list(j);
+        track_subind = find(track_list==target_track);
+        track_ind    = date_ind(track_subind);
+        %skip if track too short
+        if length(track_ind)<min_track
+            continue
+        end
+        %sort by time
+        [~,sort_ind] = sort(storm_date_list(track_ind));
+        track_ind    = track_ind(sort_ind);
+        %pass to 2dgrid, return density and u,v
+        track_grids = track_to_grid(track_grids,track_ind,storm_date_list,storm_latloncent_list,storm_subset_list,storm_ijbox_list,db_root,radar_id,r_scale,stormh5_varname,stormh5_min);
+    end
+end
+
+keyboard
+
+%loop by storm days
+%loop by tracks of length
+%for each track pair, run 2dgrid code, also calc u,v assign to 2dgrid
+
+function track_grids = track_to_grid(track_grids,track_ind,storm_date_list,storm_latloncent_list,storm_subset_list,storm_ijbox_list,db_root,radar_id,r_scale,stormh5_varname,stormh5_min)
+
+%store stormh5 fields in a cell array
+storm_data = cell(length(track_ind),1);
+
+%load storm h5 datafield from file
+for i = 1:length(track_ind)
+    %build paths to h5 from date and radar_id
+    target_date    = storm_date_list(track_ind(i));
+    target_subset  = storm_subset_list(track_ind(i));
+    target_datevec = datevec(target_date);
+    target_fn      = [num2str(radar_id,'%02.0f'),'_',datestr(target_date,'yyyymmdd_HHMMSS'),'.storm.h5'];
+    target_ffn     = [db_root,num2str(radar_id,'%02.0f'),'/',num2str(target_datevec(1)),'/',...
+        num2str(target_datevec(2),'%02.0f'),'/',num2str(target_datevec(3),'%02.0f'),'/',target_fn];
+    %read required h5 field
+    h5data         = h5read(target_ffn,['/',num2str(target_subset),'/',stormh5_varname]);
+    %rescale data
+    h5data         = double(h5data)./r_scale;
+    storm_data{i}  = h5data;
+end
+
+%density track
+blank_grid         = zeros(size(track_grids.density_grid));
+track_density_grid = blank_grid;
+
+%set
+init_ind        = track_ind(1:end-1);
+init_storm_data = storm_data(1:end-1);
+finl_ind        = track_ind(2:end);
+finl_storm_data = storm_data(2:end);
+
+for i=1:length(init_ind)
+    %create inital mask
+    init_grid  = blank_grid;
+    init_ijbox = storm_ijbox_list(init_ind(i),:);
+    init_grid(init_ijbox(1):init_ijbox(2),init_ijbox(3):init_ijbox(4)) = init_storm_data{i};
+    if strcmp(stormh5_min,'nan')
+        init_mask  = init_grid > min(init_grid(:));
+    else
+        init_mask  = init_grid >= stormh5_min;
+    end
+    %create final mask
+    finl_grid = blank_grid;
+    finl_ijbox = storm_ijbox_list(finl_ind(i),:);
+    finl_grid(finl_ijbox(1):finl_ijbox(2),finl_ijbox(3):finl_ijbox(4)) = finl_storm_data{i};
+    if strcmp(stormh5_min,'nan')
+        finl_mask  = finl_grid > min(finl_grid(:));
+    else
+        finl_mask  = finl_grid >= stormh5_min;
+    end
+    %apply convex hull
+    conv_mask  = init_mask | finl_mask;
+    conv_mask  = bwconvhull(conv_mask);
+    %append
+    track_density_grid = track_density_grid + conv_mask;
+end
+%normalise
+track_density_grid = track_density_grid>1;
+track_grids.density_grid = track_grids.density_grid+track_density_grid;
+
+% track_u_grid       = blank_grid;
+% track_v_grid       = blank_grid;
+% 
+% keyboard
+%for each cell, parse stormh5 filename and storm stormh5_varname
+%mask using stormh5_min
+%for each pair, generate convex hull of combined masks and u,v
 
 function generate_map(data_grid,data_grid_R,map_config_fn)
 
