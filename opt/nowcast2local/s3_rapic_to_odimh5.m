6a7d528ab61a2021376502881644818db2e5c7cc
Joshua S <j.soderholm@uq.edu.au>
2017-06-20 16:22:28 +1000

    added local to s3 archive builder

--- opt/nowcast2local/s3_rapic_to_odimh5.m
+++ opt/nowcast2local/s3_rapic_to_odimh5.m
@@ -1,11 +1,30 @@
 function s3_rapic_to_odimh5
 
+%WHAT: converts rapic files on s3 to odimh5 files on s3
+%rapic files names are in the folder root/radar_id/paths/.../.rapic
+%odimh5 filename radar_id and date are derived from the odimh5 file header
+%(/what)
+%odimh5 files are moved back into s3 with
+%root/radarid/yyyy/mm/dd/id_yyyymmdd_HHMM00.h5
+
+%paths
+if ~isdeployed
+    addpath('../../etc')
+    addpath('../../lib/m_lib')
+end
+mkdir('tmp')
+
+%init
 s3_input_root = 's3://roames-weather-rapic/';
 s3_input_path = 'rapic_archive/201606-09/';
 s3_output     = 's3://roames-weather-odimh5/odimh5_archive/';
-radar_id_list = [2:3];
 prefix_cmd    = 'export LD_LIBRARY_PATH=/usr/lib; ';
 log_fn        = 'matlab.log';
+config_fn     = 'config';
+
+%read config
+read_config(config_fn);
+load(['tmp/',config_fn,'.mat']);
 
 for i = 1:length(radar_id_list)
     radar_id   = radar_id_list(i);
@@ -64,9 +83,11 @@ for i = 1:length(radar_id_list)
         s3_odimh5_ffn = [s3_output,num2str(h5_radar_id,'%02.0f'),'/',num2str(h5_datevec(1)),'/',...
             num2str(h5_datevec(2),'%02.0f'),'/',num2str(h5_datevec(3),'%02.0f'),'/',odimh5_fn];
         cmd           = [prefix_cmd,'aws s3 mv ',local_odimh5_ffn,' ',s3_odimh5_ffn,' &'];
-        [sout,eout]   = unix(cmd); 
+        [~,~]         = unix(cmd); 
     end
+    utility_pushover('s3_rapic_to_odimh5',['finished radar ',num2str(radar_id)]);
 end
+utility_pushover('s3_rapic_to_odimh5',['COMPLETE fire sites ',num2str(radar_id_list)]);
 
 %log each error and pass file to brokenVOL archive
 function write_log(log_fn,type,msg)
